3327e7d9cf734bb763e4e8fb62b913e3d98399b8
Tianon Gravi <admwiggin@gmail.com>
2015-08-12 11:33:43 -0700

    Add initial .travis.yml

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
@@ -8,6 +8,7 @@ versions=( "${versions[@]%/}" )
 
 downloadable="$(curl -sSL 'https://www.elastic.co/downloads/past-releases' | sed -rn 's!.*?/downloads/past-releases/(kibana-)?[0-9]+-[0-9]+-[0-9]+">Kibana ([0-9]+\.[0-9]+\.[0-9]+)<.*!\2!gp')"
 
+travisEnv=
 for version in "${versions[@]}"; do
 	fullVersion="$(echo "$downloadable" | grep -m1 "^$version")"
 	sha1="$(curl -fsSL "https://download.elastic.co/kibana/kibana/kibana-$fullVersion-linux-x64.tar.gz.sha1.txt" | cut -d' ' -f1)"
@@ -19,4 +20,9 @@ for version in "${versions[@]}"; do
 			s/^(ENV KIBANA_SHA1) .*/\1 '"$sha1"'/;
 		' "$version/Dockerfile"
 	)
+
+	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
+
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
