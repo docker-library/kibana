9ee82ac70fba3b88550e5109e820164d6a7ffdfe
Michelle Sausa <mnikki137@gmail.com>
2018-09-27 12:43:51 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,10 @@ set -eo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-versions=( */ )
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
 versions=( "${versions[@]%/}" )
 
 tags="$(git ls-remote --tags https://github.com/elastic/kibana.git | cut -d/ -f3 | cut -d^ -f1 | cut -dv -f2 | sort -rV)"
@@ -12,23 +15,59 @@ debArch="$(dpkg --print-architecture)"
 travisEnv=
 for version in "${versions[@]}"; do
 	# major.minor.patch
-	versionMajor="${version%%.*}"
-	versionMinor="${version#$versionMajor.}"
-	[ "$versionMinor" != "$version" ] || versionMinor=
-	versionMinor="${versionMinor%%.*}"
+	majorVersion="${version%%.*}"
+	minorVersion="${version#$majorVersion.}"
+	[ "$minorVersion" != "$version" ] || minorVersion=
+	minorVersion="${minorVersion%%.*}"
 
-	fullVersion=
-	sha1=
-	if (
-		[ "$versionMajor" -eq 4 ] \
-		&& [ -n "$versionMinor" ] \
-		&& [ "$versionMinor" -ge 4 ] \
-	) || [ "$versionMajor" -gt 4 ]; then
-		if [ "$versionMajor" -eq 5 ]; then
-			repoBase='https://artifacts.elastic.co/packages/5.x/apt'
-		else
-			repoBase="http://packages.elastic.co/kibana/$version/debian"
-		fi
+
+	aptBucket="${majorVersion}.x"
+	if [ "$rcVersion" != "$version" ]; then
+		aptBucket+='-prerelease'
+	fi
+	debRepo="https://artifacts.elastic.co/packages/$aptBucket/apt"
+	tarballUrlBase='https://artifacts.elastic.co/downloads'
+	if [ "$majorVersion" -eq 2 ]; then
+		debRepo="http://packages.elasticsearch.org/elasticsearch/$aptBucket/debian"
+		tarballUrlBase='https://download.elastic.co/elasticsearch'
+	fi
+
+	fullVersion="$(curl -fsSL "$debRepo/dists/stable/main/binary-amd64/Packages" | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "elasticsearch" && $1 == "Version" && $2 ~ /^([0-9]+:)?'"$rcVersion"'/ { print $2 }' | sort -rV | head -n1)"
+	if [ -z "$fullVersion" ]; then
+		echo >&2 "warning: cannot find full version for $version"
+		continue
+	fi
+	# convert "1:5.0.2-1" over to "5.0.2"
+	plainVersion="${fullVersion%%-*}" # strip non-upstream-version
+	plainVersion="${plainVersion##*:}" # strip epoch
+	tilde='~'; plainVersion="${plainVersion//$tilde/-}" # replace '~' with '-'
+    
+    sha1= 
+
+    
+    
+    if [ $majorVersion -ge 6 ]; then
+		# Use the "upstream" Dockerfile, which rebundles the existing image from Elastic.
+		upstreamImage="docker.elastic.co/elasticsearch/kibana:$plainVersion"
+		
+		# Parse image manifest for sha
+		authToken="$(curl -fsSL 'https://docker-auth.elastic.co/auth?service=token-service&scope=repository:kibana/kibana:pull' | jq -r .token)"
+        digest="$(curl --head -fsSL -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' -H "Authorization: Bearer $authToken" "https://docker.elastic.co/v2/kibana/kibana/manifests/$plainVersion" | tr -d '\r' | gawk -F ':[[:space:]]+' '$1 == "Docker-Content-Digest" { print $2 }')"
+        
+        # Format image reference (image@sha)
+        upstreamImageDigest="$upstreamImage@$digest"
+        
+		(
+			set -x
+			sed '
+				s!%%ELASTICSEARCH_VERSION%%!'"$plainVersion"'!g;
+				s!%%UPSTREAM_IMAGE_DIGEST%%!'"$upstreamImageDigest"'!g;
+			' Dockerfile-upstream.template > "$version/Dockerfile"
+		)
+		travisEnv='\n  - VERSION='"$version VARIANT=$travisEnv"
+	else
+		repoBase='https://artifacts.elastic.co/packages/5.x/apt'
+
 		packagesUri="$repoBase/dists/stable/main/binary-$debArch/Packages"
 		debVersions="$(
 			curl -fsSL "$packagesUri" \
@@ -39,37 +78,25 @@ for version in "${versions[@]}"; do
 				| sort -rV
 		)"
 		fullVersion="$(echo "$debVersions" | head -n1)"
-	else
-		possibleVersions="$(echo "$tags" | grep "^$version\.")"
-		nonBetaVersions="$(echo "$possibleVersions" | grep -vE 'milestone|-beta|-m')"
-		# prefer full releases over beta or milestone
-		if [ "$nonBetaVersions" ]; then
-			fullVersion="$(echo "$nonBetaVersions" | head -n1)"
-		else
-			fullVersion="$(echo "$possibleVersions" | head -n1)"
+
+		if [ -z "$fullVersion" ]; then
+			echo >&2
+			echo >&2 "warning: unable to figure out 'full version' for $version"
+			echo >&2 '    skipping'
+			echo >&2
+			continue
 		fi
+		(
+			set -x
+			sed -ri '
+				s/^(ENV KIBANA_MAJOR) .*/\1 '"$version"'/;
+				s/^(ENV KIBANA_VERSION) .*/\1 '"$fullVersion"'/;
+				s/^(ENV KIBANA_SHA1) .*/\1 '"$sha1"'/;
+			' "$version/Dockerfile"
+		)
 
-		sha1="$(curl -fsSL "https://download.elastic.co/kibana/kibana/kibana-$fullVersion-linux-x64.tar.gz.sha1.txt" | cut -d' ' -f1)"
-	fi
-
-	if [ -z "$fullVersion" ]; then
-		echo >&2
-		echo >&2 "warning: unable to figure out 'full version' for $version"
-		echo >&2 '    skipping'
-		echo >&2
-		continue
-	fi
-
-	(
-		set -x
-		sed -ri '
-			s/^(ENV KIBANA_MAJOR) .*/\1 '"$version"'/;
-			s/^(ENV KIBANA_VERSION) .*/\1 '"$fullVersion"'/;
-			s/^(ENV KIBANA_SHA1) .*/\1 '"$sha1"'/;
-		' "$version/Dockerfile"
-	)
-
-	travisEnv='\n  - VERSION='"$version$travisEnv"
+		travisEnv='\n  - VERSION='"$version$travisEnv"
+	 fi
 done
 
 travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
